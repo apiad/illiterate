e1ef1ffd362207aa7f2b956671b01be67d7cf2d7
Alejandro Piad <apiad@apiad.net>
2025-07-06 11:44:12 -0400

    Passing the simplest test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use pulldown_cmark::{Parser, Tag, CodeBlockKind, Event};
+
 fn main() {
-    println!("Hello, world!");
+
+}
+
+#[derive(Debug)]
+struct Chunk {
+    language: String,
+    content: String,
+    name: String,
+    path: String,
+}
+
+
+fn extract_chunks(file_path: &str) -> Vec<Chunk> {
+    let mut chunks = Vec::new();
+
+    // open file path
+    let content = std::fs::read_to_string(file_path).unwrap();
+    let parser = Parser::new(&content);
+
+    for event in parser {
+        match event {
+            Event::Start(Tag::CodeBlock(kind)) => {
+                if let CodeBlockKind::Fenced(info) = kind {
+                    chunks.push(Chunk {
+                        name: info.to_string(),
+                        content: String::new(),
+                        path: String::new(),
+                        language: info.to_string(),
+                    })
+                }
+            }
+
+            _ => {}
+        }
+    }
+
+    return chunks;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_simple_chunk() {
+        // open tests/simple.md
+        let chunks = extract_chunks("tests/simple.md");
+
+        assert!(chunks.len() == 1);
+
+        let chunk0 = &chunks[0];
+        assert!(chunk0.language == "rust");
+    }
 }
---
+++ tests/simple.md
@@ -0,0 +1,11 @@
+# A simple markdown
+
+Here is a simple chunk of code.
+
+```rust
+fn main() {
+    println!("Hello, world!");
+}
+```
+
+Let's see how this works.
